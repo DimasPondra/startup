b2955b98bc4c028236d333c6fd081e78b40bc1aa
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2023-11-17 16:53:37 +0700

    :sparkles: campaign struct

---
+++ app/structs/campaign.go
@@ -0,0 +1,27 @@
+package structs
+
+import "time"
+
+type Campaign struct {
+	ID               int
+	Name             string
+	Slug             string
+	ShortDescription string
+	Description      string
+	GoalAmount       int
+	CurrentAmount    int
+	Perks            string
+	BackerCount      int
+	CreatedAt        time.Time
+	UpdatedAt 		 time.Time
+	UserID 			 int
+}
+
+type CampaignImage struct {
+	ID 			int
+	FileName 	string
+	IsPrimary 	int
+	CreatedAt   time.Time
+	UpdatedAt 	time.Time
+	CampaignID 	int
+}
